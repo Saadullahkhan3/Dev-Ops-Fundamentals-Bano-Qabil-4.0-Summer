1a281c03ca4706593f94d038193a9f90c69662ff
Saadullah Khan <Saadullahkhan43251@gmail.com>
2025-07-20 03:54:13 +0500

    Push the file and then will pull into wsl, doing for syncing, then will only code(until no need) into wsl only becuase we have VScode there, nice

---
+++ Assignment 2 - Shell based log analyzer and System Health Monitor/log_analyzer.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+
+# ===== LOG FILE ANALYSIS REPORT =====
+# File: /var/log/application.log
+# Analyzed on: Fri Jul 12 14:32:15 EDT 2025
+# Size: 15.4MB (16,128,547 bytes)
+
+# MESSAGE COUNTS:
+# ERROR: 328 messages
+# WARNING: log_file,253 messages
+# INFO: 8,792 messages
+
+# TOP 5 ERROR MESSAGES:
+# 182 - Database connection failed: timeout
+# 56 - Invalid authentication token provided
+# 43 - Failed to write to disk: Permission denied
+# 29 - API rate limit exceeded
+# 18 - Uncaught exception: Null pointer reference
+
+# ERROR TIMELINE:
+# First error: [2025-07-10 02:14:32] Database connection failed: timeout
+# Last error:  [2025-07-12 14:03:27] Failed to write to disk: Permission denied
+
+# Error frequency by hour:
+# 00-04: ███████ (72)
+# 04-08: ██ (23)
+# 08-12: ████████████ (120)
+# 12-16: ██████ (63)
+# 16-20: ███ (34)
+# 20-24: ████ (16)
+
+# Report saved to: log_analysis_20250712_143215.txt
+
+
+log_file = $1
+
+calculate_tag_count(tag, log_file){
+    return $(grep "$tag" $log_file)
+}
+errors_count = $(grep "error" $log_file)
+warning_count = $(grep "warning" $log_file)
+info_count = $(grep "info" $log_file)
+
+echo "Detailed error: $(grep "ERROR" $log_file | cut -d: -f1 | sort | uniq -c | sort -nr) "
+
+
+
+report = """
+Log Analyzer and System Health Monitor
+===== LOG FILE ANALYSIS REPORT =====
+File: $log_file
+Analyzed on: $(date)
+Size: $(du -h $log_file)
+
+MESSAGE COUNTS:
+ERROR: $errors_count
+WARRING: $warning_count
+INFO: $info_count
+
+TOP 5 ERROR MESSAGES:
+
+
+ERROR TIMELINE
+First error: $first_error
+Last error: $last_error
+
+Error frequency by hour:
+# 00-04: ███████ (72)
+# 04-08: ██ (23)
+# 08-12: ████████████ (120)
+# 12-16: ██████ (63)
+# 16-20: ███ (34)
+# 20-24: ████ (16)
+
+
+Report saved to: $report_file
+"""
